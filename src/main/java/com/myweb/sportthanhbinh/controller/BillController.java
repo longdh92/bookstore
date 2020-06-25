7d1b9278dea69ea24310dd86af6f6de762f663f8
ntva15z <ntva15z@gmail.com>
2020-06-25 16:16:41 +0700

    hoang update

--- src/main/java/com/myweb/sportthanhbinh/controller/BillController.java
+++ src/main/java/com/myweb/sportthanhbinh/controller/BillController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin/bill")
@@ -21,6 +23,16 @@ public class BillController {
         List<Bill> list = billReponsitory.findAll();
         model.addAttribute("bill",list);
         return "admin/bill/list";
+    }
+
+    @GetMapping("/update/{id}")
+    public String updateBillActive(ModelMap model, @PathVariable(name = "id")Long id){
+        Optional<Bill> bill = billReponsitory.findById(id);
+        if(bill.isPresent()){
+            bill.get().setStatus(2);
+            billReponsitory.save(bill.get());
+        }
+        return "redirect:/admin/bill/list";
     }
 
 }
